98b21af97fd7afaa58b975279aa2ce089beb5dbe
patrick-rush <patrick.a.rush@gmail.com>
2021-02-23 22:00:52 -0500

    commit tree 87aced59576fb4c14edaface0decfa84c80fca64

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {
+                ...state,
+                friends: [...state.friends, action.friend]
+            }
+        case "REMOVE_FRIEND":
+            console.log(action)
+            return {
+                ...state,
+                friends: [...state.friends.filter((friend) => friend.id !== action.id)]
+            }
+        default:
+            return state;
+    }
 }
